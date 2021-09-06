a79c06a831583c7784069203d289861e34cdc0d4
Galen Shirey <54954464+gjshirey@users.noreply.github.com>
2021-09-05 22:39:52 -0400

    initial commit

---
+++ preload.js
@@ -0,0 +1,23 @@
+// preload.js
+const { ipcRenderer, contextBridge} = require('electron')
+
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('info').addEventListener('click', () => {
+    ipcRenderer.invoke('info')
+  })
+  document.getElementById('minimize-btn').addEventListener('click', () => {
+    ipcRenderer.invoke('minimize')
+  })
+  document.getElementById('close-btn').addEventListener('click', () => {
+    ipcRenderer.invoke('close')
+  })
+  document.getElementById('reload-btn').addEventListener('click', () => {
+    ipcRenderer.invoke('reload')
+  })
+  if(document.getElementById('loc').innerText == 'undefined'){
+    ipcRenderer.invoke('reload')
+  }
+})
---
+++ webpack.common.js
@@ -0,0 +1,77 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/renderer.js',
+  devtool: 'inline-source-map',
+  target: 'electron-renderer',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [[
+              '@babel/preset-env', {
+                targets: {
+                  esmodules: true
+                }
+              }]]
+          }
+        }
+      },
+      // ...
+      {
+        test: /\.(scss)$/,
+        use: [{
+          // inject CSS to page
+          loader: 'style-loader'
+        }, {
+          // translates CSS into CommonJS modules
+          loader: 'css-loader'
+        }, {
+          // Run postcss actions
+          loader: 'postcss-loader',
+          options: {
+            // `postcssOptions` is needed for postcss 8.x;
+            // if you use postcss 7.x skip the key
+            postcssOptions: {
+              // postcss plugins, can be exported to postcss.config.js
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        }, {
+          // compiles Sass to CSS
+          loader: 'sass-loader'
+        }]
+      }
+      // ...
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js'],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'build', 'js'),
+  },
+};
+
+/*{
+  test: [/\.s[ac]ss$/i, /\.css$/i],
+  use: [
+    // Creates `style` nodes from JS strings
+    'style-loader',
+    // Translates CSS into CommonJS
+    'css-loader',
+    // Compiles Sass to CSS
+    'sass-loader',
+  ],
+}*/
